1fc5a624f7c10bd5dd84640e9133e8f96ad80227
Dominik Jančík <hi@domj.net>
2026-04-22 22:46:39 +0300

    refactor(app): extract stored loop action helpers into stored_loops module

--- src/app.rs
+++ src/app.rs
@@ -63,6 +63,8 @@ mod routing_ui;
 mod shell_ui;
 #[path = "app/shell_layout.rs"]
 mod shell_layout;
+#[path = "app/stored_loops.rs"]
+mod stored_loops;
 #[path = "app/timeline_fx_ui.rs"]
 mod timeline_fx_ui;
 #[path = "app/timeline_layout.rs"]
@@ -89,6 +91,10 @@ use mapping_lookup::mapping_target_lookup_input;
 use discoverability_ui::track_indicator_target;
 use mapping_ui::{direct_mapping_key_label, mapping_target_label_for_action};
 use mapping_input::{midi_learn_label, midi_mapping_matches_event};
+use stored_loops::{
+    clear_stored_loop_slot_index, recall_stored_loop_slot_index, store_stored_loop_slot_index,
+    stored_loop_slot_color, stored_loop_slot_recall_action,
+};
 pub(crate) use types::DiscoverabilityTarget;
 use types::{
     ActionDiscoverabilitySummary, ActiveMappingTargetLookup, AppOverlay, DirectMappingMode,
@@ -4058,75 +4064,6 @@ fn indexed_all_notes(track: &Track) -> Vec<(usize, crate::project::MidiNote)> {
 
 fn transport_strip_height() -> u32 {
     34
-}
-
-fn recall_stored_loop_slot_index(action: AppAction) -> Option<usize> {
-    match action {
-        AppAction::RecallStoredLoopSlot1 => Some(0),
-        AppAction::RecallStoredLoopSlot2 => Some(1),
-        AppAction::RecallStoredLoopSlot3 => Some(2),
-        AppAction::RecallStoredLoopSlot4 => Some(3),
-        AppAction::RecallStoredLoopSlot5 => Some(4),
-        AppAction::RecallStoredLoopSlot6 => Some(5),
-        AppAction::RecallStoredLoopSlot7 => Some(6),
-        AppAction::RecallStoredLoopSlot8 => Some(7),
-        _ => None,
-    }
-}
-
-fn store_stored_loop_slot_index(action: AppAction) -> Option<usize> {
-    match action {
-        AppAction::StoreCurrentLoopToSlot1 => Some(0),
-        AppAction::StoreCurrentLoopToSlot2 => Some(1),
-        AppAction::StoreCurrentLoopToSlot3 => Some(2),
-        AppAction::StoreCurrentLoopToSlot4 => Some(3),
-        AppAction::StoreCurrentLoopToSlot5 => Some(4),
-        AppAction::StoreCurrentLoopToSlot6 => Some(5),
-        AppAction::StoreCurrentLoopToSlot7 => Some(6),
-        AppAction::StoreCurrentLoopToSlot8 => Some(7),
-        _ => None,
-    }
-}
-
-fn clear_stored_loop_slot_index(action: AppAction) -> Option<usize> {
-    match action {
-        AppAction::ClearStoredLoopSlot1 => Some(0),
-        AppAction::ClearStoredLoopSlot2 => Some(1),
-        AppAction::ClearStoredLoopSlot3 => Some(2),
-        AppAction::ClearStoredLoopSlot4 => Some(3),
-        AppAction::ClearStoredLoopSlot5 => Some(4),
-        AppAction::ClearStoredLoopSlot6 => Some(5),
-        AppAction::ClearStoredLoopSlot7 => Some(6),
-        AppAction::ClearStoredLoopSlot8 => Some(7),
-        _ => None,
-    }
-}
-
-fn stored_loop_slot_recall_action(slot_index: usize) -> Option<AppAction> {
-    match slot_index {
-        0 => Some(AppAction::RecallStoredLoopSlot1),
-        1 => Some(AppAction::RecallStoredLoopSlot2),
-        2 => Some(AppAction::RecallStoredLoopSlot3),
-        3 => Some(AppAction::RecallStoredLoopSlot4),
-        4 => Some(AppAction::RecallStoredLoopSlot5),
-        5 => Some(AppAction::RecallStoredLoopSlot6),
-        6 => Some(AppAction::RecallStoredLoopSlot7),
-        7 => Some(AppAction::RecallStoredLoopSlot8),
-        _ => None,
-    }
-}
-
-fn stored_loop_slot_color(slot_index: usize) -> Color {
-    match slot_index % STORED_LOOP_SLOT_COUNT {
-        0 => Color::RGB(214, 124, 118),
-        1 => Color::RGB(214, 176, 98),
-        2 => Color::RGB(184, 206, 108),
-        3 => Color::RGB(114, 198, 174),
-        4 => Color::RGB(114, 168, 214),
-        5 => Color::RGB(144, 138, 214),
-        6 => Color::RGB(204, 132, 206),
-        _ => Color::RGB(210, 144, 164),
-    }
 }
 
 fn loop_regions_intersect(a: crate::timeline::LoopRegion, b: crate::timeline::LoopRegion) -> bool {
